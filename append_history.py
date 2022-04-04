36f8ab137b916768dedb0c399db7237c8535e63a
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2022-04-04 10:45:21 -0400

    Update append_history.py

--- append_history.py
+++ append_history.py
@@ -2,7 +2,7 @@ from microprediction import MicroReader
 import os
 import random
 from copy import deepcopy
-CSV_PATH = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'csv' + os.path.sep
+RECENT_CSV = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + 'recent' + os.path.sep
 import matplotlib.pyplot as plt
 import time
 import pathlib
@@ -11,28 +11,41 @@ import time
 mr = MicroReader()
 STREAMS = [ n for n in mr.get_stream_names() ]
 
+MB_LIMIT = 20.  # Github file size limit is 25MB to be safe 
+
 def append_random():
       """ Pick a random stream and update the history """
       name = random.choice(STREAMS)
-      chrono_csv = CSV_PATH + name.replace('.json','.csv') 
+      chrono_csv = RECENT_CSV + name.replace('.json','.csv') 
       # Find time of update
       fchrono = pathlib.Path(chrono_csv)
       if fchrono.exists():
-           chrono_df = pd.read_csv(chrono_csv)
+           existing_df = pd.read_csv(chrono_csv)
            modification_time = os.path.getmtime(chrono_csv)
            seconds = time.time()-modification_time 
-           print('Seconds since modification: '+str(seconds))
-           chrono = [ (t,v) for t,v in zip(chrono_df['time'].values, chrono_df['value'].values) ] 
-           appended = mr.append_chrono(chrono=chrono, seconds=seconds, name=name)
+           print('Seconds since modification: '+str(seconds))   
+           existing = [ (t,v) for t,v in zip(existing_df['time'].values, existing_df['value'].values) ] 
+           appended = mr.append_chrono(chrono=existing, name=name)
       else:
          seconds_since_modification = 24*60*60
-         appended = mr.get_lagged(name=name, count=2000) 
+         appended = list(reversed( mr.get_lagged(name=name, count=2000) ))
 
       df_new = pd.DataFrame(columns=['time','value'])
       df_new['time'] = [t for (t,v) in appended]
       df_new['value'] = [v for (t,v) in appended] 
       df_new.to_csv(chrono_csv) 
       
+      # Check size of file 
+      mb = os.path.getsize(chrono_csv)/(1024*1024)
+      if mb>MB:
+         # If we approach the file size limit, shuffle to the archive 
+         older = df_new[:-10]
+         newer = df_new[-10:] 
+         older_csv = chrono_csv.replace('recent','older').replace('.csv','_'+time.strftime('%Y%m%d')+'.csv')
+         older.to_csv(older_csv)
+         newer.to_csv(newer_csv)
+            
+         
 
 if __name__=='__main__':
     for _ in range(20):
